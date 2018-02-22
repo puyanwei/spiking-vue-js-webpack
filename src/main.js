17a3b07def574821422155082a6a615140dc728f
Puyan Wei <puyanwei@gmail.com>
2018-02-22 16:52:02 +0000

    Vue Files and the Root Component - changed the default component App.vue is the default template component that gets exported into main.js, which gets rendered to the page

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue"; // component template imported
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  el: "#app",
+  render: h => h(App) // renders the component to the div id app
+});
