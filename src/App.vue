3537033485a6a45a68d60cf113505506effbc57f
Puyan Wei <puyanwei@gmail.com>
2018-02-23 11:50:12 +0000

    Header, Footer, Main Example - setup environment

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
-  <div>
-<h1>{{ title }}</h1>
-<street-fighter></street-fighter>
-  </div>
+
 </template>
 
 <script>
-  import StreetFighter from "./StreetFighter.vue"; // nesting our component locally
-
   export default {
-    components: {
-      "street-fighter": StreetFighter
-    },
     data() {
-      return {
-        title: "Badass App!"
-      }
+      return {}
     }
   }
 </script>
 
-<style scoped>
-  h1 {
-    color: purple
-  }
-
-  /* will style every element on the page even if it isn't in root, unless you add the scoped attribute into the style app */
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue"; // component template imported
-import StreetFighter from "./StreetFighter.vue";
 
 new Vue({
   el: "#app",
