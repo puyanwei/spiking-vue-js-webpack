f24db438b292d967e60f5676ff412e098f8212b9
Puyan Wei <puyanwei@gmail.com>
2018-02-25 15:40:28 +0000

    Slots - creating a form template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
     <div>
       <form-helper>
-        <h2 slot="title">I'm the slot title</h2>
+        <h2 slot="title">{{ title }}</h2>
         <p slot="text">I'm the paragraph text for the slot</p>
       </form-helper>
     </div>
 </template>
 
 <script>
-  import formHelper from "./components/formHelper.vue";
+import formHelper from "./components/formHelper.vue";
 
-  export default {
-    components: {
-      "form-helper": formHelper
-    },
-    data() {
-      return {
-      }
-    },
-    methods: {
-
-    }
-  };
+export default {
+  components: {
+    "form-helper": formHelper
+  },
+  data() {
+    return {
+      title: "I'm a dynamic slot title"
+    };
+  },
+  methods: {}
+};
 </script>
 
 <style>
+
 </style>
--- src/components/formHelper.vue
+++ src/components/formHelper.vue
@@ -1,26 +1,44 @@
 <template>
   <div>
-    <slot name="title"></slot>
-    <h1>I am the form helper</h1>
-    <slot name="text"></slot>
+    <h1>Please fill out the form</h1>
+    <form>
+      <div id="form-header">
+        <slot name="form-header"></slot>
+      </div>
+
+      <div id="form-fields">
+        <slot name="form fields">
+      </div>
+
+      <div id="form-controls">
+        <slot name="form controls">
+      </div>
+
+      <div id="useful-links"></div>
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+
+    </form>
   </div>
 </template>
 
 <script>
-
 export default {
-  components: {
-
-  },
+  components: {},
   data() {
-    return {
-    }
+    return {};
   },
-  methods: {
-
-  }
+  methods: {}
 };
 </script>
 
 <style scoped>
-</style>
+h1 {
+  color: red;
+}
+</style>;
